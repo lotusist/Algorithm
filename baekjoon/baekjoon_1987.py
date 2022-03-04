22b69d58f7359297b6afcac4fa2873a015a1bec5
lotusist <lotusist@naver.com>
2022-03-04 17:34:21 +0900

    baekjoon

---
+++ baekjoon/baekjoon_1987.py
@@ -0,0 +1,13 @@
+# 알파벳
+import sys
+R, C = map(int, sys.stdin.readline().split())
+
+data = []
+for _ in range(R):
+    data.append(list(sys.stdin.readline().strip()))
+
+
+
+#print(data)
+#print(data[0][2])
+#print(data[1][1])
