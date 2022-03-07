5d171692a9947e1dd0a447972a9cb8a28677e9eb
lotusist <83007583+lotusist@users.noreply.github.com>
2022-03-07 15:53:26 +0900

    [Bronze III] Title: 분산처리, Time: 64 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,24 @@
+import sys
+
+T = int(input())
+for _ in range(T):
+    a, b = map(int, sys.stdin.readline().split())
+    c = a%10 # a를 10으로 나누었을 때 나머지
+    if c == 0:
+        print(10)
+    elif c == 1 or c == 5 or c == 6:
+        print(c)
+    elif c == 4 or c == 9:
+        if b % 2 == 1:
+            print(c)
+        else:
+            print((c*c)%10)
+    else:
+        if b % 4 == 1:
+            print(c)
+        elif b % 4 == 2:
+            print((c**2)%10)
+        elif b % 4 == 3:
+            print((c**3)%10)
+        else:
+            print((c**4)%10)
